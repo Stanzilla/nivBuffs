a2cad0a877e27cb2c3c2fbb89ed3ce4a3bb341af
maat <calebzor@gmail.com>
2013-01-06 11:46:45 +0000

    initial import

---
+++ Locales/enUS.lua
@@ -0,0 +1,76 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("nivBuffs", "enUS", true)
+
+-- general
+L["Name"] = true
+L["Index"] = true
+L["Time"] = true
+L["General"] = true
+L["Sort method"] = true
+L["How to sort the buffs/debuffs"] = true
+L["Show weapon enchaments"] = true
+L["This change requires reloading UI to take effect"] = true
+L["Show duration spiral"] = true
+L["Show duration bar"] = true
+L["Show duration text"] = true
+L["Colored border"] = true
+L["Highlight debuffs and weapon enchants with a different border color"] = true
+L["Border brightness"] = true
+L["Brightness of the default non-colored icon border ( 0 -> black, 1 -> white )"] = true
+L["Blink start time"] = true
+L["A buff/debuff icon will blink when it expires in less than X seconds, set to 0 to disable"] = true
+L["Blink speed"] = true
+L["Blinking speed as number of blink cycles per second"] = true
+L["Use Masque (ButtonFacade)"] = true
+L["Duration position"] = true
+L["Top"] = true
+L["Bottom"] = true
+L["Left"] = true
+L["Right"] = true
+L["Duration horizontal offset"] = true
+L["Duration vertical offset"] = true
+L["Stack horizontal offset"] = true
+L["Stack vertical offset"] = true
+L["Changing some of the values might require reloading the UI to take effect. Watch out for the red text in the options tooltip description!"] = true
+L["Duration font"] = true
+L["None"] = true
+L["Monochrome"] = true
+L["Outline"] = true
+L["Thick outline"] = true
+L["Duration font outline"] = true
+L["Duration font size"] = true
+L["Duration font color"] = true
+L["Stack font"] = true
+L["Stack outline"] = true
+L["Stack size"] = true
+L["Stack font color"] = true
+L["Duration"] = true
+L["Stacks"] = true
+L["Abbreviated"] = true
+L["Duration time format"] = true
+L["HH:MM:SS"] = true
+L["Profile"] = true
+L["For any kind of profile change to fully take effect, you need to reaload UI!"] = true
+
+
+L["Buffs"] = true
+L["Debuffs"] = true
+L["Anchor X offset"] = true
+L["Nagative or positive numbers expected"] = true
+L["Anchor y offset"] = true
+L["Show/Hide Anchor"] = true
+L["Show/Hide test anchor frame"] = true
+L["Icons/Row"] = true
+L["Number of Rows"] = true
+L["Scale"] = true
+L["Horizontal offset"] = true
+L["Vertical offset"] = true
+L["Horizontal row offset"] = true
+L["Positive values adjust to the right, negative values adjust to the left"] = true
+L["Vertical row offset"] = true
+L["Positive values adjust upwards, negative values adjust downwards"] = true
+L["Sort type"] = true
+L["Ascending"] = true
+L["Descending"] = true
+
+
+
